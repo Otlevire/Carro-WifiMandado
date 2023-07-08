d919fd43036dff2af8fae5a30589a0052db027ff
otlevire <eriveltoclenio@gmail.com>
2023-07-09 00:22:02 +0100

    Segunda Entrega

--- Código Microcontrolador - ESP32/CARRO_ROBOTIZADO_4WD/CARRO_ROBOTIZADO_4WD_HOST_V2.ino
+++ Código Microcontrolador - ESP32/CARRO_ROBOTIZADO_4WD/CARRO_ROBOTIZADO_4WD_HOST_V2.ino
@@ -1,10 +1,10 @@
 /**************************************************
-   * AUTHOR: ERIVELTO CLÉNIO DA COSTA E SILVA
-   * FOR: RODRIGO CONTROLE REMOTO DE CARRO ROBOTIZADO 4WD
-   * CREATED AT: 03-06-2023
-   * Objectivo: Controlar um carro robotizado de 4 rodas por meio do uma página web
+     AUTHOR: ERIVELTO CLÉNIO DA COSTA E SILVA
+     FOR: RODRIGO CONTROLE REMOTO DE CARRO ROBOTIZADO 4WD
+     CREATED AT: 03-06-2023.
+     Objectivo: Controlar um carro robotizado de 4 rodas por meio do uma página web
    no computador ou celular, isso via WIFI.
-   
+
 
   Principais Componentes usados:
  *** 1-ESP32 (1)
@@ -16,20 +16,27 @@
 ////////////////// Labraries Used  ////////////////
 #include <WiFi.h>                             /////
 #include <AsyncTCP.h>                         /////
+#include <Ultrasonic.h>                       /////
 #include <ESPAsyncWebServer.h>                /////
-///////////////////////////////////////////////////                                                                                                                                                                                                                                         
+///////////////////////////////////////////////////
 
 ///////////////////Definitions/////////////////////
-#define LED 2                            //////////
-#define MOTOR_E_A 15 
-#define MOTOR_E_B 4
-#define MOTOR_D_A 5
-#define MOTOR_D_B 18
+#define LED 2                                 /////
+#define MOTOR_E_A 15                          /////
+#define MOTOR_E_B 4                           /////
+#define MOTOR_D_A 5                           /////
+#define MOTOR_D_B 18                          /////
+#define BUZZER 13                             /////
+#define FAROL_TRAZEIRO_ESQUERDO 19            /////
+#define FAROL_TRAZEIRO_DIREITO 21             /////
+#define TRIGGER 22                            /////
+#define ECHO 23                               /////
 ///////////////////////////////////////////////////
+
 
 /////////////  NETWORK CONFIGURATIONS /////////////
 const char* ssid = "CAR";                     /////
-const char* password = "123456789";           /////
+const char* password = "987654321";           /////
 ///////////////////////////////////////////////////
 
 ////////// NETWORK USENAME AND PASSWORD ///////////
@@ -38,17 +45,21 @@ const char* PAGE_PASSWORD = "admin";          /////
 ///////////////////////////////////////////////////
 
 ////////// VARIABLES USED IN THE PROJECT //////////
-unsigned long int tempoDelay=0;               /////
-byte cont=0;                                  /////
-char estadoCarro = 'A';                       /////
+unsigned long int tempoDelay = 0;             /////
+byte cont = 0;                                /////
+char estadoCarro = 'D';                       /////
 ///////////////////////////////////////////////////
 
 
 ///////////////  OBJECTS DEFINITIONS  /////////////
+Ultrasonic ultrasonico(TRIGGER, ECHO);        /////
 AsyncWebServer server(80);                    /////
 ///////////////////////////////////////////////////
 
 //////////////////////////////// FUNCTION DEFINITIONS  ////////////////////////////////////////////////
+void tocarBuzina();
+void desativarBuzina();
+boolean hasObstaculo;
 void carroFrente();
 void carroEsquerda();
 void carroDireita();
@@ -60,43 +71,28 @@ void initialConfigurations();                                                   
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html lang="pt-pt">
 
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="Página para controle de Carro Robotizado!">
+    <meta name="keywords" content="Página, Carro,Robotico, Robotizado, 4WD, Controle, WIFI, Projecto">
+    <meta name="author" content="Erivelto Clénio da Costa e Silva">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carro - Controle de Carro</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./bootstrap.min.css">
+    <title>MyCar - Controle de Carro</title>
+
     <style>
         * {
             padding: 0;
             margin: 0;
             box-sizing: border-box;
         }
-
-        header,
-        main {
-            width: 80%;
-            margin: auto;
-        }
-
-        header,
-        .tempo,
-        .horario {
-            /* background-color: rgb(245, 247, 247); */
-            /* background: linear-gradient(to bottom, #4e83d8, #061120); */
-        }
-
-        main {
+         body {
             background: linear-gradient(to bottom, #0f60ca, #123772);
         }
 
@@ -105,66 +101,94 @@ const char index_html[] PROGMEM = R"rawliteral(
             height: 100px;
         }
 
-        header h3 {
-            margin-top: 50px;
+        header h2 {
+            color: white;
+            
+            margin-top: 5px;
             text-align: center;
+            
         }
 
-        .data h5,
-        .hora h5 {
-            background: linear-gradient(to bottom, #4e83d8, #061120);
+        .centralizado {
+            width: 80%;
+            margin: auto;
         }
 
-        .data div,
-        .hora div,
+        .contentor {
+            height: 460px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .contentor-botao {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
         .botao {
-            /* background: linear-gradient(to bottom, #838181, #c4d1e4, #838181); */
-            background: linear-gradient(to bottom, #4e83d8, #061120);
+            background-color: #111b29;
+            width: 50%;
+            color: white;
+            padding: 15px;
+            border-radius: 5px;
+            border-width: 0px;
         }
 
-        .data p,
-        .hora p {
-            font-weight: bold;
-            font-size: x-large;
+        .lado-a-lado {
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
         }
 
-        .horario {
-            color: white
+        .m-vertical {
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+
+        .m-horizontal {
+            margin-left: 10px;
+            margin-right: 10px;
         }
     </style>
 </head>
 
-<body class="container-fluid w-75 mt-5 mx-auto">
-
-    <header class="d-flex">
-        <img class="mx-5" src="" alt="">
-        <h3 class="mx-auto mb-0">Controle <strong> do Carro</strong></h3>
+<body>
+<header class="centralizado">
+        <h2>Controle <strong> do Carro</strong></h2>
     </header>
-
-    <main class="text-light">
-        <div class="d-grid gap-2 col-6 mx-auto mt-3">
-            <h4 class="text-center">Controles</h4>
-            <button id="botaoFrente" class="btn btn-dark mt-2 ">
+    <main class="centralizado contentor">
+        <div class="contentor-botao">
+            <button id="botaoFrente" class="botao">
                 Frente
             </button>
         </div>
 
-
-        <div class="horario mt-3">
-            <div class="d-flex pb-2 botoes w-100">
-                <button id="botaoEsquerda" class="w-50 btn btn-dark mt-2">
+        <div class="contentor-botao m-vertical">
+            <div class="lado-a-lado">
+                <button id="botaoEsquerda" class="botao  m-horizontal">
                     Esquerda
                 </button>
-                <button id="botaoDireita" class="w-50 btn btn-dark  ms-2 mt-2">
+                <button id="botaoDireita" class="botao  m-horizontal">
                     Direita
                 </button>
             </div>
         </div>
-        <div class="d-grid gap-2 col-6 mx-auto mt-3">
-            <button id="botaoRecuar" class="btn btn-dark mt-2 ">
+
+        <div class="contentor-botao m-vertical">
+            <button id="botaoRecuar" class="botao">
                 Recuar
             </button>
-            <button id="botaoParar" type="button" class="btn btn-dark mt-2">
+        </div>
+
+        <div class="contentor-botao m-vertical">
+            <button id="botaoParar" type="button" class="botao">
                 Parar
             </button>
         </div>
@@ -172,17 +196,18 @@ const char index_html[] PROGMEM = R"rawliteral(
         <div class="row">
             <div class="col form-check">
                 <label class="form-check-label" for="flexCheckDefault">
-                    
+
                 </label>
             </div>
             <div class=" col form-check">
                 <input class="form-check-input" type="checkbox" value="" id="checkPresenca" nome="estadoPresenca">
                 <label class="form-check-label" for="estadoPresenca">
-                    S.Prensença
+                    Sen.Obstaculo
                 </label>
             </div>
         </div>
     </main>
+
 
     <script>
         function requisicaoGet(stringRequisicao) {
@@ -195,16 +220,17 @@ const char index_html[] PROGMEM = R"rawliteral(
             req.open("GET", "/" + stringRequisicao, true);
             req.send(null);
         }
+
         window.onload = function () {
             document.getElementById("botaoFrente").addEventListener("click", () => { requisicaoGet("carroFrente"); });
             document.getElementById("botaoEsquerda").addEventListener("click", () => { requisicaoGet("carroEsquerda"); });
             document.getElementById("botaoDireita").addEventListener("click", () => { requisicaoGet("carroDireita"); });
             document.getElementById("botaoParar").addEventListener("click", () => { requisicaoGet("carroParar"); });
             document.getElementById("botaoRecuar").addEventListener("click", () => { requisicaoGet("carroRecuar"); });
-            document.getElementById("checkPresenca").onchange = ()=>{
+            document.getElementById("checkPresenca").onchange = () => {
                 let valorCheck = (document.getElementById("checkPresenca").checked) ? "1" : "0";
-                requisicaoGet("estadoPresenca?estado="+valorCheck);
-            }           
+                requisicaoGet("estadoPresenca?estado=" + valorCheck);
+            }
         }
     </script>
 </body>
@@ -212,7 +238,8 @@ const char index_html[] PROGMEM = R"rawliteral(
 </html>
 )rawliteral";
 
-////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
 void setup(){
   initialConfigurations();
   wifiConfigurations();
@@ -220,26 +247,41 @@ void setup(){
   Serial.println("\nSISTEMA DE DOMÓTICA!");
 }
 
-////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////  
 void loop() {
-  if(millis()-tempoDelay>1500)
+  if(millis()-tempoDelay>500)
   {
       tempoDelay = millis();
       digitalWrite(LED, !digitalRead(LED));
+      long distancia = ultrasonico.read();
+      Serial.println("OSTACULO A:"+String(distancia)+"cm");
+      if(distancia <20 && estadoCarro=='L')
+      {
+        carroParar();
+        tocarBuzina(); 
+      }
+      else{
+        desativarBuzina();  
+      }
   }
-  wifiConfigurations();
   delay(20);
 }
 
 /////////////////////////////////////////////////////////
 void initialConfigurations(){
   pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(FAROL_TRAZEIRO_ESQUERDO, OUTPUT);
+  pinMode(FAROL_TRAZEIRO_DIREITO, OUTPUT);
   pinMode(MOTOR_E_A, OUTPUT);
   pinMode(MOTOR_E_B, OUTPUT);
   pinMode(MOTOR_D_A, OUTPUT);
   pinMode(MOTOR_D_B, OUTPUT);
   
   digitalWrite(LED, LOW);
+  digitalWrite(BUZZER, LOW);
+  digitalWrite(FAROL_TRAZEIRO_ESQUERDO, LOW);
+  digitalWrite(FAROL_TRAZEIRO_DIREITO, LOW);
   digitalWrite(MOTOR_E_A, LOW);
   digitalWrite(MOTOR_E_B, LOW);
   digitalWrite(MOTOR_D_A, LOW);
@@ -250,25 +292,18 @@ void initialConfigurations(){
 /////////////////////////////////////////////////////////
 void wifiConfigurations()
 {
-  if(WiFi.status() == WL_CONNECTED) return;
   Serial.println();
-  Serial.print("CONECTANDO A WIFI:");  
-  Serial.println(ssid);
-  Serial.print("PROCURANDO");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  for(int i=0; WiFi.status() != WL_CONNECTED;i++){
-    digitalWrite(LED, !digitalRead(LED));
-    delay(150); Serial.print(".");  
-    if(i==100)
-      ESP.restart();
-  }
-  // Print ESP Local IP Address
-  Serial.print("\nCONECTADO AO WIFI NO IP:");
-  Serial.println(WiFi.localIP());
+  Serial.println("Configuring access point...");
+
+  // You can remove the password parameter if you want the AP to be open.
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
 }
 
-////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////
 void serverHandlers()
 {
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -309,12 +344,12 @@ void serverHandlers()
     digitalWrite(LED, !digitalRead(LED));
     carroDireita();
     AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", "Ok");
-    response->addHeader("carroEsquerda","Carro Indo para Direita");
+    response->addHeader("carroDireita","Carro Indo para Direita");
     request->send(response);
   });
 
   server.on("/carroRecuar", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    Serial.println("\n#####Carro Indo para Recuar#####");
+    Serial.println("\n#####Carro Recuar#####");
     digitalWrite(LED, !digitalRead(LED));
     carroRecuar();
     AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", "Ok");
@@ -329,9 +364,14 @@ void serverHandlers()
     ///estadoPresenca?estado=true
     AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", "Ok");
     if(request->getParam("estado")->value().toInt())
-      estadoCarro = 'A';
-    else
-      estadoCarro = 'M';
+    {
+      estadoCarro = 'L';
+      Serial.println(" ## SENSOR DE OBSTACULOS LIGADO! ");
+    }else
+    {
+      estadoCarro = 'D';
+      Serial.println(" ## SENSOR DE OBSTACULOS DESLIGADO! ");
+    }
     response->addHeader("estadoPresenca", "ESTADO DO SENSOR DE PRESENÇA ALTERADO!");
     request->send(response);
   });
@@ -347,6 +387,8 @@ void carroFrente()
   digitalWrite(MOTOR_E_B, LOW);
   digitalWrite(MOTOR_D_A, HIGH);
   digitalWrite(MOTOR_D_B, LOW);
+  digitalWrite(FAROL_TRAZEIRO_ESQUERDO, LOW);
+  digitalWrite(FAROL_TRAZEIRO_DIREITO, LOW);
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -356,24 +398,30 @@ void carroRecuar()
   digitalWrite(MOTOR_E_B, HIGH);
   digitalWrite(MOTOR_D_A, LOW);
   digitalWrite(MOTOR_D_B, HIGH);
+  digitalWrite(FAROL_TRAZEIRO_ESQUERDO, HIGH);
+  digitalWrite(FAROL_TRAZEIRO_DIREITO, HIGH);
 }
 
 ////////////////////////////////////////////////////////////////////////
 void carroEsquerda()
 {
-  digitalWrite(MOTOR_E_A, LOW);
+  digitalWrite(MOTOR_E_A, HIGH);
   digitalWrite(MOTOR_E_B, LOW);
-  digitalWrite(MOTOR_D_A, HIGH);
+  digitalWrite(MOTOR_D_A, LOW);
   digitalWrite(MOTOR_D_B, LOW);
+  digitalWrite(FAROL_TRAZEIRO_ESQUERDO, LOW);
+  digitalWrite(FAROL_TRAZEIRO_DIREITO, HIGH);
 }
 
 ////////////////////////////////////////////////////////////////////////
 void carroDireita()
 {
-  digitalWrite(MOTOR_E_A, HIGH);
+  digitalWrite(MOTOR_E_A, LOW);
   digitalWrite(MOTOR_E_B, LOW);
-  digitalWrite(MOTOR_D_A, LOW);
+  digitalWrite(MOTOR_D_A, HIGH);
   digitalWrite(MOTOR_D_B, LOW);
+  digitalWrite(FAROL_TRAZEIRO_ESQUERDO, HIGH);
+  digitalWrite(FAROL_TRAZEIRO_DIREITO, LOW); 
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -383,4 +431,17 @@ void carroParar()
   digitalWrite(MOTOR_E_B, LOW);
   digitalWrite(MOTOR_D_A, LOW);
   digitalWrite(MOTOR_D_B, LOW);
+  digitalWrite(FAROL_TRAZEIRO_ESQUERDO, HIGH);
+  digitalWrite(FAROL_TRAZEIRO_DIREITO, HIGH);
+  tocarBuzina();
+}
+
+void tocarBuzina()
+{
+  digitalWrite(BUZZER, HIGH);
+}
+
+void desativarBuzina()
+{
+  digitalWrite(BUZZER, LOW);
 }
